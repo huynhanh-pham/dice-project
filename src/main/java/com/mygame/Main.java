4c230e1ebf1f7289ecd186dfb8d9f734086fe8ac
huynhanh-pham <thupham1893@gmail.com>
2025-10-24 18:53:58 -0500

    setup

---
+++ src/main/java/com/mygame/Main.java
@@ -0,0 +1,51 @@
+package com.mygame;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.renderer.RenderManager;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+import com.jme3.system.AppSettings;
+
+/**
+ * This is the Main Class of your Game. You should only do initialization here.
+ * Move your Logic into AppStates or Controls
+ * @author normenhansen
+ */
+public class Main extends SimpleApplication {
+
+    public static void main(String[] args) {
+        //AppSettings settings = new AppSettings(true);
+       // settings.setResolution(1640,1480);
+        //settings.setFullscreen(true);
+        DiceRollApp app = new DiceRollApp();
+       // app.setSettings(settings);
+        app.setShowSettings(false);
+        
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        Box b = new Box(1, 1, 1);
+        Geometry geom = new Geometry("Box", b);
+
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setColor("Color", ColorRGBA.Blue);
+        geom.setMaterial(mat);
+
+        rootNode.attachChild(geom);
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        //TODO: add update code
+    }
+
+    @Override
+    public void simpleRender(RenderManager rm) {
+        //TODO: add render code
+    }
+}
+
